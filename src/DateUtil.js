f81cf8038b928e5dfac57e9eefd46210b55a56e7
Luke Kozak <kozakluke@gmail.com>
2018-10-31 10:59:15 +0100

    Set DateUtil as deprecated

--- src/DateUtil.js
+++ src/DateUtil.js
@@ -2,6 +2,7 @@ class DateUtil
 {
     static dateDiff(date1, date2)
     {
+        console.log('DateUtil::dateDiff is deprecated');
         const utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate()),
               utc2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
         return Math.floor((utc2 - utc1) / DateUtil.MS_PER_DAY);
@@ -9,6 +10,7 @@ class DateUtil
     
     static addDays(date, days)
     {
+        console.log('DateUtil::addDays is deprecated');
         return new Date(date.getTime() + days * DateUtil.MS_PER_DAY);
     }
 }
